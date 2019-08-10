08ecf87a3c64bcbc32b68a87dfc7226386489b7b
Chris <chris@chrispturner.com>
2019-08-10 19:29:07 -0400

    Add project files.

---
+++ NCAA_FB_Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NCAA_FB_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
